87baa4cb9b9b3f04d634a17894fb9f56281dbad4
Ryan <ryan.poh@gmail.com>
2019-08-04 00:03:56 +0800

    Adding Semantic Library and test

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Omni</h1>
+        <EventDashboard />
       </div>
     );
   }
