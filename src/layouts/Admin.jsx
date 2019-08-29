cb46941a9af15aa4cd485f7a927d900d6a0270eb
Ryan Poh <ryan.poh@gmail.com>
2019-08-29 12:31:09 +0800

    Update Admin.jsx

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -112,7 +112,7 @@ class Dashboard extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={routes}
-          logoText={"Creative Tim"}
+          logoText={"OMNI"}
           logo={logo}
           image={this.state.image}
           handleDrawerToggle={this.handleDrawerToggle}
